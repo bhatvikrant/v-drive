31db46df9e3f55bc433933c74af3fc9f40e3d075
Vikrant Bhat <vik.bhat223@gmail.com>
2021-08-06 21:02:29 +0530

    Files UI updates

--- src/components/File.tsx
+++ src/components/File.tsx
@@ -1,3 +1,9 @@
+import Image from "next/image";
+import Link from "next/link";
+
+// SVGs
+import DownloadIcon from "../../public/svgs/download.svg";
+
 // TS INTERFACES
 interface Props {
 	file: any;
@@ -5,11 +11,29 @@ interface Props {
 
 const File: React.FC<Props> = props => {
 	const { file } = props;
+	console.log("file:", file);
 
 	return (
-		<div>
-			<a href={file.url} target="_blank" rel="noreferrer">
+		<div className="flex flex-row items-center justify-center border rounded w-72 sm:w-64">
+			<Image
+				src={file.url}
+				alt={file.name}
+				height="150"
+				width="250"
+				className="object-cover rounded-md"
+			/>
+
+			<a
+				className="p-2 text-gray-500 truncate w-72 sm:w-64 link link-hover"
+				href={file.url}
+				target="_blank"
+				rel="noreferrer"
+			>
 				{file.name}
+			</a>
+
+			<a href={file.url} download className="p-2 cursor-pointer">
+				<DownloadIcon />
 			</a>
 		</div>
 	);
--- src/components/FilesAndFoldersDisplay.tsx
+++ src/components/FilesAndFoldersDisplay.tsx
@@ -43,7 +43,7 @@ const FilesAndFoldersDisplay: React.FC<Props> = props => {
 					folderState.childFiles.length > 0 && <hr className='my-8' />}
 
 				{folderState.childFiles.length > 0 && (
-					<div className="flex gap-4">
+					<div className="flex flex-wrap items-center justify-center gap-4">
 						{folderState.childFiles.map(childFile => (
 							<div key={childFile.id}>
 								<File file={childFile} />
