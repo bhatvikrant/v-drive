2937a57dfe9f2fb6bc69b02922bc57f251ada1a8
Vikrant Bhat <vik.bhat223@gmail.com>
2021-09-18 02:40:09 +0530

    packages updated, firebase compat used

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,44 +1,44 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
-import 'firebase/storage'
-import 'firebase/analytics'
-import 'firebase/performance'
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+import "firebase/compat/analytics";
+import "firebase/compat/performance";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-}
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
 
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig)
-    // Check that `window` is in scope for the analytics module!
-    if (typeof window !== 'undefined') {
-        // Enable analytics. https://firebase.google.com/docs/analytics/get-started
-        if ('measurementId' in firebaseConfig) {
-            firebase.analytics()
-            firebase.performance()
-        }
-    }
-    /* eslint-disable */
-    console.log('Firebase was successfully init.')
+	firebase.initializeApp(firebaseConfig);
+	// Check that `window` is in scope for the analytics module!
+	if (typeof window !== "undefined") {
+		// Enable analytics. https://firebase.google.com/docs/analytics/get-started
+		if ("measurementId" in firebaseConfig) {
+			firebase.analytics();
+			firebase.performance();
+		}
+	}
+	/* eslint-disable */
+	console.log("Firebase was successfully init.");
 }
 
-const firestore = firebase.firestore()
+const firestore = firebase.firestore();
 
 export const db = {
-    folders: firestore.collection("folders"),
-    files: firestore.collection("files"),
-    formatDoc: doc => ({
-        id: doc.id,
-        ...doc.data(),
-    }),
-    getCurrentTimeStamp: firebase.firestore.FieldValue.serverTimestamp
-}
+	folders: firestore.collection("folders"),
+	files: firestore.collection("files"),
+	formatDoc: doc => ({
+		id: doc.id,
+		...doc.data(),
+	}),
+	getCurrentTimeStamp: firebase.firestore.FieldValue.serverTimestamp,
+};
 
-export default firebase
+export default firebase;
